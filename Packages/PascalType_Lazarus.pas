e740bbc5d4c015e20484f5ac1c2cd596796a403c
cwbudde <cwbudde@50a0e610-f8c1-47bb-876c-b11951c5d1ff>
2010-04-10 17:52:11 +0000

    * fixed accidentially uploading the wrong file

--- Packages/PascalType_Lazarus.pas
+++ Packages/PascalType_Lazarus.pas
@@ -8,14 +8,19 @@ unit PascalType_Lazarus;
 interface
 
 uses
-    PT_Component, PT_Interpreter, PT_ResourceStrings, PT_Tables, 
-  PT_TablesOpenType, PT_TablesOpenTypeFeatures, PT_TablesOpenTypeLanguages, 
-  PT_TablesOpenTypeScripts, PT_TablesOptional, PT_Types, LazarusPackageIntf;
+    PT_ByteCodeInterpreter, PT_CharacterMap, PT_Component, PT_Interpreter, 
+  PT_PanoseClassifications, PT_Rasterizer, PT_ResourceStrings, 
+  PT_TableDirectory, PT_Tables, PT_TablesApple, PT_TablesBitmap, 
+  PT_TablesFontForge, PT_TablesOpenType, PT_TablesOpenTypeFeatures, 
+  PT_TablesOpenTypeLanguages, PT_TablesOpenTypeScripts, PT_TablesOptional, 
+  PT_TablesPostscript, PT_TablesShared, PT_TablesTrueType, PT_Types, 
+  PT_UnicodeNames, LazarusPackageIntf;
 
 implementation
 
 procedure Register; 
 begin
+  RegisterUnit('PT_Component', @PT_Component.Register); 
 end; 
 
 initialization
